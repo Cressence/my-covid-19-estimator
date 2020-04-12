493ab25b08a807a80f23e2e88b6814f8c55547d1
Cressence <cressencey@gmail.com>
2020-04-12 23:14:57 +0100

    Complete challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,19 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => ({
+  data,
+  estimate: {
+    impact: {
+      currentlyInfected: (data.reportedCases * 10),
+      get infectionsByRequestime() {
+        return this.currentlyInfected * 1024;
+      }
+    },
+    severeImpact: {
+      currentlyInfected: (data.reportedCases * 50),
+      get infectionsByRequestime() {
+        return this.currentlyInfected * 1024;
+      }
+    }
+  }
+});
 
 export default covid19ImpactEstimator;
