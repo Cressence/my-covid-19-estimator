b61861d05c96d66d1bdcfdac1166ed280044794c
Cressence <cressencey@gmail.com>
2020-04-13 00:38:29 +0100

    Add Math.floor to the weeks and months cnverter

--- src/functions.js
+++ src/functions.js
@@ -2,10 +2,10 @@ export const convertDaysBy3 = (days) => Math.floor(days / 3);
 
 export const convertWeeksByDays = (weeks) => {
   const days = Math.floor(weeks * 7);
-  return days / 3;
+  return Math.floor(days / 3);
 };
 
 export const convertMonthsByDays = (months) => {
   const days = Math.floor(months * 30);
-  return days / 3;
+  return Math.floor(days / 3);
 };
