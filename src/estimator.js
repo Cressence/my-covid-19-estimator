0aedf766e0d035cd930a6364e107c0865d4fd8b1
Cressence <cressencey@gmail.com>
2020-04-12 23:16:06 +0100

    Change name from  infectionsByRequestime to infectionsByRequesTime

--- src/estimator.js
+++ src/estimator.js
@@ -3,13 +3,13 @@ const covid19ImpactEstimator = (data) => ({
   estimate: {
     impact: {
       currentlyInfected: (data.reportedCases * 10),
-      get infectionsByRequestime() {
+      get infectionsByRequesTime() {
         return this.currentlyInfected * 1024;
       }
     },
     severeImpact: {
       currentlyInfected: (data.reportedCases * 50),
-      get infectionsByRequestime() {
+      get infectionsByRequesTime() {
         return this.currentlyInfected * 1024;
       }
     }
